ad47cea6066be33a512fd5282025e9b20f6f7ac6
Wyatt Roberts <wyatt6of7@gmail.com>
2022-04-23 13:18:45 -0700

    Sounds

--- src/main/java/arvem/sky/Sky.java
+++ src/main/java/arvem/sky/Sky.java
@@ -1,6 +1,7 @@
 package arvem.sky;
 
 import net.fabricmc.api.ModInitializer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +16,6 @@ public class Sky implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 	}
 }
