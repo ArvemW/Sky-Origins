dc62703adfe93f32fa328c5fea9ee7d0a77a5f44
Wyatt Roberts <wyatt6of7@gmail.com>
2022-04-22 20:17:40 -0700

    Origins

--- src/main/java/arvem/sky/mixin/SkyMixin.java
+++ src/main/java/arvem/sky/mixin/SkyMixin.java
@@ -1,16 +1,17 @@
-package net.fabricmc.example.mixin;
+package arvem.sky.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import arvem.sky.Sky;
+
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class SkyMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Sky.LOGGER.info("I'm not afraid of the dark, I'm afraid of not being alone in the dark.");
 	}
 }
