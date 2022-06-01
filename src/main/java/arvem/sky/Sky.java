20cf91999039aff95f8b9d3074c74661d3a2d3b9
Wyatt Roberts <wyatt6of7@gmail.com>
2022-06-01 14:21:38 -0700

    added knockback and tidying

--- src/main/java/arvem/sky/Sky.java
+++ src/main/java/arvem/sky/Sky.java
@@ -1,11 +1,6 @@
 package arvem.sky;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
-import net.minecraft.util.registry.Registry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,14 +10,12 @@ public class Sky implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("sky");
-	public static final Item WEAK_WL = new Item(new Item.Settings().group(ItemGroup.COMBAT).maxCount(1).fireproof().rarity(Rarity.UNCOMMON));
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registry.ITEM, new Identifier("sky", "weak_wl"), WEAK_WL);
 		Sounds.registerflap();
 		Sounds.registerboost();
 		Sounds.registerwhoosh();
